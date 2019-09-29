4d0c527f1cd278a58e0c935db531452c8b99cf94
gmf123 <894697547@qq.com>
2019-09-29 22:44:12 +0800

    [add]nio,bio,aio练习

--- src/test/java/cn/fmxn/nio/NioDemo.java
+++ src/test/java/cn/fmxn/nio/NioDemo.java
@@ -2,6 +2,13 @@ package cn.fmxn.nio;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * @description:
  * @author: gmf
@@ -10,9 +17,58 @@ import org.junit.Test;
  * @modified By:
  */
 public class NioDemo {
+    //往本地文件中写数据
     @Test
-    public void test1(){
-
+    public void test1() throws Exception {
+        String meg = "hello noi 文本文件";
+        FileOutputStream fos = new FileOutputStream("demo.txt");
+        FileChannel channel = fos.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        buffer.put(meg.getBytes());
+        buffer.flip();
+        channel.write(buffer);
+        fos.close();
+    }
+    //从本地文件中读数据
+    @Test
+    public void test2() throws Exception{
+        File file = new File("demo.txt");
+        FileInputStream fis = new FileInputStream(file);
+        FileChannel channel = fis.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate((int) file.length());
+        channel.read(buffer);
+        System.out.println(new String(buffer.array()));
+        fis.close();
+    }
+    //bio复制文件
+    @Test
+    public void test3() throws Exception{
+        FileInputStream fis = new FileInputStream("E:\\IDEA_workplace\\io\\demo.txt");
+        FileOutputStream fos = new FileOutputStream("E:\\IDEA_workplace\\io\\demo2.txt");
+        byte[] b = new byte[1024];
+        while (true){
+            int i = fis.read(b);
+            if(i==-1){
+                break;
+            }
+            fos.write(b,0,i);
+        }
+        fis.close();
+        fos.close();
+    }
+    //nio复制文件
+    @Test
+    public void test4() throws Exception{
+        FileInputStream fis = new FileInputStream("E:\\IDEA_workplace\\io\\demo.txt");
+        FileOutputStream fos = new FileOutputStream("E:\\IDEA_workplace\\io\\demo3.txt");
+        FileChannel sourceCh = fis.getChannel();
+        FileChannel destCh = fos.getChannel();
+        //从目标通道中复制数据到当前通道
+        //destCh.transferFrom(sourceCh,0,sourceCh.size());
+        //把数据从当前通道复制给目标通道
+        sourceCh.transferTo(0,sourceCh.size(),destCh);
+        sourceCh.close();
+        destCh.close();
     }
 
 }
